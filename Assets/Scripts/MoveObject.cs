dccad9f0af5578a14badb808f32ad6c75581efba
redfenix <alexanderkiryshin@gmail.com>
2019-05-20 23:00:36 +0300

    garbage cleaning

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -5,7 +5,6 @@ namespace Assets.Scripts
     public class MoveObject:MonoBehaviour
     {
         public Vector2 speed;
-       // private GameObject gameobject;
         float maxX, minX;
 
         void Start()
@@ -15,16 +14,8 @@ namespace Assets.Scripts
             maxX =Screen.width;
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed.x, speed.y));
         }
-
-        void Update()
-        {
-           
-        }
-
         void FixedUpdate()
         {
-           // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * speed.x, Time.deltaTime * speed.y));
-            //gameObject.transform.position = Vector3.MoveTowards(transform.position, new Vector3(Screen.width, transform.position.y, 0), 0.5f);
             if (transform.position.x > maxX)
             {
                 transform.position = new Vector2(maxX, transform.position.y);
@@ -36,11 +27,6 @@ namespace Assets.Scripts
                 transform.position = new Vector2(minX, transform.position.y);
                 gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2 * speed.x, 0));
             }
-           // else if (transform.position.x < minX) transform.position = new Vector2(minX, transform.position.y);
         }
-       /* public void Update()
-        {
-            gameobject.transform.
-        }*/
     }
 }

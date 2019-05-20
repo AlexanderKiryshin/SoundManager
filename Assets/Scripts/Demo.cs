1bab1874792e17d3fb0e057e76f6d89c5c62cd6a
redfenix <alexanderkiryshin@gmail.com>
2019-05-20 22:57:32 +0300

    initial commit

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+   public class Demo:MonoBehaviour
+   {
+       public GameObject[] targets;
+       public Dropdown nameSound;
+       public Slider volumeSlider;
+       public Dropdown typeSound;
+       public Dropdown gameobjectDropDown;
+       public Toggle loop;
+       public Toggle MoveWithObjectToggle;
+       public Button playButton;
+       public Text countSound;
+       public Dropdown playableSounds;
+       public Button stopButton;
+       public Button perfomanceButton;
+       private float volume;
+       private bool is2DSound;
+       private bool isLooped;
+       private bool moveWithObject;
+       private GameObject selectedtarget;
+       private string soundname;
+       private SoundManager soundManager;
+       void Start()
+       {
+           soundManager = FindObjectOfType<SoundManager>();
+           SoundManager.SoundsChangePlayingCountEvent += OnChangeCountSoindPlaying;
+           nameSound.onValueChanged.AddListener(delegate
+           {
+               OnNameSoundChange();
+           });
+           volumeSlider.onValueChanged.AddListener(delegate
+           {
+               OnChangeVolume();
+           });
+           typeSound.onValueChanged.AddListener(delegate
+           {
+               OnChangeTypeSound();
+           });
+           gameobjectDropDown.onValueChanged.AddListener(delegate
+           {
+               OnchangeGameobject();
+           });
+           loop.onValueChanged.AddListener(delegate
+           {
+               OnChangeLoopToggle();
+           });
+           MoveWithObjectToggle.onValueChanged.AddListener(delegate
+           {
+               OnChangeMoweWithObjectToggle();
+           });
+           playButton.onClick.AddListener(delegate
+           {
+               OnClickPlayButton();
+           });
+           stopButton.onClick.AddListener(delegate
+           {
+               OnClickStopButton();
+           });
+           perfomanceButton.onClick.AddListener(delegate
+           {
+               PerfomanceTest();
+           });
+            OnNameSoundChange();
+           OnChangeVolume();
+           OnChangeTypeSound();
+           OnchangeGameobject();
+           OnChangeLoopToggle();
+           OnChangeMoweWithObjectToggle();
+       }
+
+        public void OnNameSoundChange()
+       {
+           switch (nameSound.value )
+           {
+                case 0: soundname = "Garage Foley - 1"; break;
+                case 1: soundname = "Garage Foley - 2"; break;
+                case 2: soundname = "Garage Foley - 3"; break;
+                case 3: soundname = "Garage Foley - 4"; break;
+                case 4: soundname = "Garage Foley - 5"; break;
+                case 5: soundname = "Garage Foley - 6"; break;
+                case 6: soundname = "Garage Foley - 7"; break;
+                case 7: soundname = "Garage Foley - 8"; break;
+                case 8: soundname = "Garage Foley - 9"; break;
+                case 9: soundname = "Garage Foley - 10"; break;
+            }
+       }
+
+        public void OnChangeCountSoindPlaying()
+        {
+            countSound.text = "Всего воспроизводится: "+soundManager.GetCountSoundsPlaying;
+        }
+       public void OnChangeVolume()
+       {
+           volume = volumeSlider.value;
+       }
+
+       public void OnChangeTypeSound()
+       {
+           switch (typeSound.value)
+           {
+               case 0:
+                   is2DSound = true; break;
+               case 1:
+                   is2DSound = false;break;
+           }
+       }
+
+       public void OnchangeGameobject()
+       {
+           switch (gameobjectDropDown.value)
+           {
+               case 0:
+                   selectedtarget = targets[0]; break;
+               case 1:
+                   selectedtarget = targets[1]; break;
+            }
+       }
+
+       public void OnChangeLoopToggle()
+       {
+           isLooped = loop.isOn;
+       }
+       public void OnChangeMoweWithObjectToggle()
+       {
+           moveWithObject = MoveWithObjectToggle.isOn;
+       }
+
+       public void OnClickPlayButton()
+       {
+           if (is2DSound)
+           {
+               soundManager.PlaySound(soundname, volume, isLooped);
+           }
+           else
+           {
+               soundManager.Play3DSound(soundname,selectedtarget,moveWithObject,volume,isLooped);
+           }
+           UpdateSoundList();
+        }
+
+       private IEnumerable<string> soundList;
+
+       public void OnClickStopButton()
+       {
+           soundManager.StopSound(playableSounds.value);
+           UpdateSoundList();
+       }
+
+       public void UpdateSoundList()
+       {
+           soundList = soundManager.GetPlayableSounds();
+            playableSounds.options.Clear();
+            foreach (var sound in soundList)
+            {
+                playableSounds.options.Add(new Dropdown.OptionData(sound));
+            }
+
+            if (playableSounds.options.Count > 0)
+            {
+                playableSounds.captionText.text = playableSounds.options[0].text;
+            }            
+       }
+
+       public void PerfomanceTest()
+       {
+           for (int i = 0; i < 300; i++)
+           {
+               if (is2DSound)
+               {
+                   soundManager.PlaySound(soundname, volume, isLooped);
+               }
+               else
+               {
+                   soundManager.Play3DSound(soundname, selectedtarget, moveWithObject, volume, isLooped);
+               }
+           }
+           UpdateSoundList();
+        }
+    }
+}
---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MoveObject:MonoBehaviour
+    {
+        public Vector2 speed;
+       // private GameObject gameobject;
+        float maxX, minX;
+
+        void Start()
+        {
+            Time.fixedDeltaTime = 0.02f;
+            minX = 0;
+            maxX =Screen.width;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed.x, speed.y));
+        }
+
+        void Update()
+        {
+           
+        }
+
+        void FixedUpdate()
+        {
+           // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(Time.deltaTime * speed.x, Time.deltaTime * speed.y));
+            //gameObject.transform.position = Vector3.MoveTowards(transform.position, new Vector3(Screen.width, transform.position.y, 0), 0.5f);
+            if (transform.position.x > maxX)
+            {
+                transform.position = new Vector2(maxX, transform.position.y);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2*speed.x, 0));
+            }
+
+            if (transform.position.x < minX)
+            {
+                transform.position = new Vector2(minX, transform.position.y);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2 * speed.x, 0));
+            }
+           // else if (transform.position.x < minX) transform.position = new Vector2(minX, transform.position.y);
+        }
+       /* public void Update()
+        {
+            gameobject.transform.
+        }*/
+    }
+}
